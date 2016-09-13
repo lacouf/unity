4b9d727364dbbc3d6ae6e9f462050061dc0752ac
Francois Lacoursiere <flacoursiere@premieresloges.ca>
2016-09-13 18:49:07 -0400

    2nd commit

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour {
+
+	public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+		print(ballPrefab);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Instantiate(ballPrefab);
+		}
+	
+	}
+}
