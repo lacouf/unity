819154c9c1ea9c0d64da95e45c6891b0407e18b8
Francois Lacoursiere <flacoursiere@premieresloges.ca>
2016-09-22 09:46:05 -0400

    Splash scene

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	static int GAMESCENE = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			SceneManager.LoadScene(GAMESCENE);
+		}
+
+	}
+}
